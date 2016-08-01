346a1d84d55d75cf26d8532f3392201cfea7db57
ykaygusuzSMS <yakup.kaygusuz@smsmt.com>
2016-08-01 10:28:34 +1000

    Initial Page

---
+++ Test.js
@@ -0,0 +1,22 @@
+const http = require('http'),
+ fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.write(html);
+  res.end();
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+});
