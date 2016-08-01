147163b34f3ed463e67953ab2111efcf0fc3631e
ykaygusuzSMS <yakup.kaygusuz@smsmt.com>
2016-08-01 14:20:02 +1000

    ejs imp

--- express-serv.js
+++ express-serv.js
@@ -1,19 +1,16 @@
 var express = require('express');
-var app = express();
-var fs = require('fs');
+var app = express();    
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }     
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  //res.send('Hello World!');
-  res.write(html);
+app.get('/', function(request, response) {
+  response.render('index');
 });
 
 app.listen(process.env.PORT||8080, function () {
   console.log('Example app listening on port '+process.env.PORT||8080+'!');
   console.log('process port is '+process.env.PORT);
 });
-});
+
+// views is directory for all template files
